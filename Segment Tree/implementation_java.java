c5867ae54d380b6942ab78c8f385f376977abcab
halaSebbah <37476298+halaSEBBAH@users.noreply.github.com>
2020-04-06 18:43:48 +0100

    added lazy propagation

--- Segment Tree/implementation_java.java
+++ Segment Tree/implementation_java.java
@@ -3,7 +3,7 @@ public class Segment_Tree {
 		
 	static int[] Tree;
 	static int[] Arr;
-	
+	static int[] lazy;
 	
 	static int Compute(int a , int b) {return Math.max(a, b);}
 	
@@ -53,7 +53,44 @@ public class Segment_Tree {
 		}
 	}
 	
+	static void updateRangeLazy(int node ,int start ,int end ,int l, int r ,int val) {
+		if(lazy[node] != 0) {
+			Tree[node] += (end-start+1)*lazy[node];
+			if(start != end) {
+				lazy[2*node] += lazy[node];
+				lazy[2*node+1] += lazy[node];
+			}
+		}
+		lazy[node] = 0;
+		if(r < start || end < l) return ;
+		else if(l<=start && end<=r) {
+			Tree[node]+=(end-start+1)*val;
+			if(start != end) {
+				lazy[2*node] += val;
+				lazy[2*node+1] += val;
+			}
+			return;
+		}
+		int mid = (start+end)/2;
+		updateRangeLazy(2*node,start,mid , l, r , val);
+		updateRangeLazy(2*node,start,mid , l, r , val);
+		Tree[node] = Tree[2*node]+Tree[2*node+1];		
+	}
 	
+	
+	
+	static int queryRangeLazy(int node, int start, int end, int l, int r) {
+		if(r < start || end < l) return 0;
+		if(lazy[node] != 0) {
+			Tree[node] += (end-start+1)*lazy[node];
+			if(start != end) {
+				lazy[2*node] += lazy[node];
+				lazy[2*node+1] += lazy[node];
+			}
+		}
+		return r;
+		
+	}
 	
 	
 	public static void main(String[] args) {
@@ -61,12 +98,22 @@ public class Segment_Tree {
 		Arr = new int[] {2,5,8,9,4};
 		int n = Arr.length;
 		Tree = new int[2*n+5];
-		build(1,0,n-1);
-		updateRange(1,0,n-1,1,3, -8);
-		System.out.println(query(1,-1,6,0,n-1));
 		
-	}
-	
-	
+		lazy = new int[2*n+5];
+		Arrays.fill(lazy,0);
+		
+		build(1,0,n-1);
+		/*updateRange(1,0,n-1,1,3, -8);
+		System.out.println(query(1,-1,6,0,n-1));*/
+		
+		updateRangeLazy(1,0,n-1,1,3,-8);
+		
+		List<List<Integer>> res =  new LinkedList<>();
+		res.add(new LinkedList<Integer>() {{add(1);add(3);}});
+		System.out.println(res.contains(new LinkedList<Integer>() {{add(1);add(3);}}));
+		
+		
+		
+	}	
 	
 }
